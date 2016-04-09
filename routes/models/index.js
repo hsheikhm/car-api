617af670b38c3c66ddbd5f2758e0fc16db5df833
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-09 17:08:39 +0100

    added code to check if modelId in url is valid

--- routes/models/index.js
+++ routes/models/index.js
@@ -2,6 +2,20 @@ var models = require('express').Router();
 var all = require('./all');
 var single = require('./single');
 var cars = require('./cars');
+var data = require('../../data.json');
+
+models.param('modelId', function(req, res, next, value){
+  var model = data.models.find(function(model){
+    return model.id === Number(value);
+  });
+
+  if(model){
+    req['model'] = model;
+    next();
+  } else {
+    res.status(404).send('Invalid model ID');
+  }
+});
 
 models.get('/', all);
 

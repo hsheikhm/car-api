e744cab1261535e93fa50e70e1f5ea49cf9c9d1a
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-09 17:30:26 +0100

    refactored code in single.js files

--- routes/models/single.js
+++ routes/models/single.js
@@ -1,9 +1,6 @@
 var data = require('../../data.json');
 
 module.exports = function(req, res){
-  var modelId = Number(req.params.modelId);
-  var model = data.models.find(function(model){
-    return model.id === modelId;
-  });
+  var model = req.model;
   res.status(200).json({ model });
 };

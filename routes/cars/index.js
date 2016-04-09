c1a2e6823559f85a1b3d725dbe47492e2ecea7ed
Hamza Sheikh <hsheikhm@gmail.com>
2016-04-09 17:12:42 +0100

    added code to check if carId in url is valid

--- routes/cars/index.js
+++ routes/cars/index.js
@@ -1,6 +1,20 @@
 var cars = require('express').Router();
 var all = require('./all');
 var single = require('./single');
+var data = require('../../data.json');
+
+cars.param('carId', function(req, res, next, value){
+  var car = data.cars.find(function(car){
+    return car.id === Number(value);
+  });
+
+  if(car){
+    req['car'] = car;
+    next();
+  } else {
+    res.status(404).send('Invalid car ID');
+  }
+});
 
 cars.get('/', all);
 
